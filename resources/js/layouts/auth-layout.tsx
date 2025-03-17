7421012cdec92b72e5abeba819a21ce2fa04dbdb
Your Name <yourmail@mail.com>
2025-03-17 11:24:47 +0800

    fixed: zoom login

--- resources/js/layouts/auth-layout.tsx
+++ resources/js/layouts/auth-layout.tsx
@@ -4,8 +4,8 @@ export default function AuthLayout({ children, title, description, ...props }: {
         //     {children}
         // </AuthSplitLayout>\
         <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-t from-[#c9ccdf] to-[#d6e1eb]">
-            <div className="mx-auto grid h-[calc(100vh-4rem] w-[calc(100vw-4rem] grid-cols-2 overflow-hidden rounded-3xl bg-white shadow-2xl">
-                <div className="hidden h-full w-full items-center justify-center bg-gradient-to-t from-[#35aecd] to-[#3e4a93] md:flex">
+            <div className="mx-auto grid h-[calc(100vh-4rem] w-[calc(100vw-4rem] grid-cols-3 overflow-hidden rounded-3xl bg-white shadow-2xl">
+                <div className="col-span-2 hidden h-full w-full items-center justify-center bg-gradient-to-t from-[#35aecd] to-[#3e4a93] md:flex">
                     <img src="/images/login-image.png" alt="login image" />
                 </div>
                 <div className="s flex w-full items-center justify-center bg-white">{children}</div>
