635ba41ba6cf881b679ad6a942a9e776639f044c
Your Name <yourmail@mail.com>
2025-03-17 11:08:11 +0800

    sd

--- resources/js/layouts/auth-layout.tsx
+++ resources/js/layouts/auth-layout.tsx
@@ -4,9 +4,9 @@ export default function AuthLayout({ children, title, description, ...props }: {
         //     {children}
         // </AuthSplitLayout>\
         <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-t from-[#c9ccdf] to-[#d6e1eb]">
-            <div className="mx-auto flex h-[600px] items-center justify-center overflow-hidden rounded-3xl bg-white shadow-2xl md:w-8/12">
+            <div className="mx-auto flex max-h-[600px] items-center justify-center overflow-hidden rounded-3xl bg-white shadow-2xl md:w-8/12">
                 <div className="hidden h-full w-full items-center justify-center bg-gradient-to-t from-[#35aecd] to-[#3e4a93] md:flex">
-                    <img src="/images/login-image.png" alt="" />
+                    <img src="/images/login-image.png" alt="login image" />
                 </div>
                 <div className="flex w-full items-center justify-center bg-white p-10">{children}</div>
             </div>
