7e83b095013fe85fe04de74923fd8d7e62dc7de1
Your Name <yourmail@mail.com>
2025-03-26 10:39:29 +0800

    add build file

---
+++ public/build/assets/loader-circle-US0VLYFl.js
@@ -0,0 +1,6 @@
+import{j as e}from"./app-tI2J1YsO.js";import{a as i}from"./createLucideIcon-DVLgItX8.js";function d({children:t,title:r,description:c,...a}){return e.jsx("div",{className:"flex h-screen w-screen items-center justify-center bg-gradient-to-t from-[#c9ccdf] to-[#d6e1eb]",children:e.jsxs("div",{className:"mx-auto grid h-[calc(100vh-4rem] w-[calc(100vw-4rem] grid-cols-3 overflow-hidden rounded-3xl bg-white shadow-2xl",children:[e.jsx("div",{className:"col-span-2 hidden h-full w-full items-center justify-center bg-gradient-to-t from-[#35aecd] to-[#3e4a93] md:flex",children:e.jsx("img",{src:"/images/login-image.png",alt:"login image"})}),e.jsx("div",{className:"s flex w-full items-center justify-center bg-white",children:t})]})})}/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],n=i("LoaderCircle",s);export{d as A,n as L};
---
+++ public/build/assets/usePermissions-DZwOunDz.js
@@ -0,0 +1,1 @@
+const s=r=>r.reduce((e,n)=>{const t=n.name.split("-")[0].toUpperCase();return e[t]||(e[t]=[]),e[t].push(n),e},{});export{s as u};
